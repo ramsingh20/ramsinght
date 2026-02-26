99cc855c8c13c6ce542faccbb07e6f4d8e531fb3
Ramsingh20 <ram02june2003@gmail.com>
2026-02-26 23:43:36 +0530

    fix: R3F: Hooks can only be used within the Canvas

--- src/components/Common/ThreeScene.tsx
+++ src/components/Common/ThreeScene.tsx
@@ -4,21 +4,26 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
 import { useRef } from "react";
 
-export default function ThreeScene() {
+// animation must run *inside* the Canvas context, so we use a child component
+function AnimatedStars() {
   const starsRef = useRef<any>(null);
 
   useFrame(() => {
     if (starsRef.current) {
       starsRef.current.rotation.x += 0.0005;
-      starsRef.current.rotation.y += 0.00010;
+      starsRef.current.rotation.y += 0.0001;
       starsRef.current.rotation.x -= 0.00015;
       starsRef.current.rotation.y -= 0.0005;
     }
   });
 
+  return <Stars ref={starsRef} />;
+}
+
+export default function ThreeScene() {
   return (
     <Canvas>
-      <Stars ref={starsRef} />
+      <AnimatedStars />
     </Canvas>
   );
 }
